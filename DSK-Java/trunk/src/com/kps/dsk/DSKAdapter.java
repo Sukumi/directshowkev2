dc2ddeff41dedeacb07740418dab2bcd8193613e
K.P.Sullivan@64kps.co.uk <K.P.Sullivan@64kps.co.uk@52f3ec68-f052-262d-1a2b-1160b071a96f>
2011-03-06 19:45:50 +0000

    Safety first

---
+++ DSK-Java/trunk/src/com/kps/dsk/DSKAdapter.java
@@ -0,0 +1,15 @@
+package com.kps.dsk;
+
+public class DSKAdapter implements IDSKListener {
+
+    @Override
+    public void stateChanged(final DSKState oldState, final DSKState newState) {
+    // Override if required.
+    }
+
+    @Override
+    public void videoProgressed() {
+    // Override if required.
+    }
+
+}
